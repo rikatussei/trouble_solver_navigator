24488273761d63c5447253b6079ed406133aafe1
@rikatussei <yohei.arisu@Gmail.com>
2024-07-09 18:55:47 +0900

    アソシエーションを設定

--- db/migrate/20240709094615_create_comments.rb
+++ db/migrate/20240709094615_create_comments.rb
@@ -1,8 +1,10 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-
+      t.string :content, null: false
       t.timestamps
+      t.references :user, null: false, foreign_key: true
+      t.references :event, null: false, foreign_key: true
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_23_083338) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_09_094615) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,16 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_23_083338) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "comments", charset: "utf8", force: :cascade do |t|
+    t.string "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "event_id", null: false
+    t.index ["event_id"], name: "index_comments_on_event_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "events", charset: "utf8", force: :cascade do |t|
@@ -71,5 +81,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_23_083338) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "events"
+  add_foreign_key "comments", "users"
   add_foreign_key "events", "users"
 end
