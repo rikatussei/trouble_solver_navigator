0030d2999877d34920d49680135e53a331387f7b
@rikatussei <yohei.arisu@Gmail.com>
2024-07-10 19:55:15 +0900

    eventモデルの記述修正

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,23 +1,22 @@
 class Event < ApplicationRecord
-  validates :title, presence: true
-  validates :description, presence: true
-  validates :occurred_on, presence: true, inclusion: {
+  # バリデーション
+  validates :title, :description, :occurred_on, :location, :images, presence: true
+  validates :occurred_on, inclusion: {
     in: (Date.new(1950, 1, 1)..Date.new(2200, 12, 31)),
     message: 'must be between January 1, 1950 and December 31, 2200'
   }
-  validates :location, presence: true
-  validates :images, presence: true
+  validates :cause_type_id, numericality: { other_than: 1, message: "can't be blank"}
 
-  # belongs_to :user
+  # アソシエーション
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
-  # has_many :user_events
-  # has_many :users, through: :user_events
   has_many :comments, dependent: :destroy
-  # has_many :support_tickets
-  has_one_attached :images
+  has_many_attached :images
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :cause_type
-  validates :cause_type_id, numericality: { other_than: 1, message: "can't be blank"} 
 
+  # 以下のアソシエーションは将来の利用予定
+  # has_many :user_events
+  # has_many :users, through: :user_events
+  # has_many :support_tickets
 end
