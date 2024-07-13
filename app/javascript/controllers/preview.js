559256c53300485733a48e623c894c93c238e78f
@rikatussei <yohei.arisu@Gmail.com>
2024-07-13 16:59:16 +0900

    古いプレビューが存在する場合は削除

--- app/javascript/controllers/preview.js
+++ app/javascript/controllers/preview.js
@@ -22,8 +22,12 @@ document.addEventListener('turbo:load', function(){
   const fileField = document.querySelector('input[type="file"][name="post[image]"]');
   // input要素で値の変化が起きた際に呼び出される関数
   fileField.addEventListener('change', function(e){
-    console.log("input要素で値の変化が起きました");
-    console.log(e.target.files[0]);
+    // 古いプレビューが存在する場合は削除
+    const alreadyPreview = document.querySelector('.preview');
+    if (alreadyPreview) {
+      alreadyPreview.remove();
+    };
+    
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
 
