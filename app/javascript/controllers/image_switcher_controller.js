9c09a5fb8fddf6e47fce0e5fa308494512cbcb5b
@rikatussei <yohei.arisu@Gmail.com>
2024-07-15 17:22:04 +0900

    ビューファイルの構造を大幅変更

---
+++ app/javascript/controllers/image_switcher_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="image-switcher"
+export default class extends Controller {
+  static targets = ["image"];
+
+  connect() {
+    console.log("ImageSwitcher controller connected");
+  }
+
+  switch(event) {
+    const index = event.target.dataset.index;
+    const mainImage = document.getElementById("main-event-image");
+    const newSrc = this.imageTargets[index].src;
+    mainImage.src = newSrc;
+  }
+}
