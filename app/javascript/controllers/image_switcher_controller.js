fd52505d27a56c7320ba5f6b8db2067dd60c6b2d
@rikatussei <yohei.arisu@Gmail.com>
2024-07-16 17:41:10 +0900

    記述の修正

--- app/javascript/controllers/image_switcher_controller.js
+++ app/javascript/controllers/image_switcher_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="image-switcher"
 export default class extends Controller {
-  static targets = ["image"];
+  static targets = ["image"]
 
   connect() {
     console.log("ImageSwitcher controller connected");
