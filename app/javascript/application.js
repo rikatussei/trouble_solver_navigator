b0a6e349d51adfb7ed57d84c0453efe3c7e9e640
@rikatussei <yohei.arisu@Gmail.com>
2024-07-17 17:49:26 +0900

    再度修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,11 @@
 import { Application } from "@hotwired/stimulus";
-import { definitionsFromContext } from "stimulus/webpack-helpers"; // 修正箇所
+import { definitionsFromContext } from "stimulus/webpack-helpers";  // 修正箇所
 import "@hotwired/turbo-rails";
 import "controllers";
 import "./controllers/preview";
 import InfiniteScrollController from "./controllers/infinite_scroll_controller";
 
 const application = Application.start();
-const context = require.context("./controllers", true, /\.js$/);
+const context = require.context("controllers", true, /\.js$/);  // 修正箇所
 application.load(definitionsFromContext(context));
 application.register("infinite-scroll", InfiniteScrollController);
-
