07977ff3bab749c0b74919b2eb43d1b15cfda2d1
@rikatussei <yohei.arisu@Gmail.com>
2024-07-18 12:46:33 +0900

    pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"の記述を削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   def show
     logger.debug "Params: #{params.inspect}"
-    @user = User.find(params[:id])
+    @user = User.find_by(id: params[:id])  # Use find_by instead of find
+    if @user.nil?
+      # Handle user not found case (e.g., redirect to login page)
+      return redirect_to new_user_session_path, alert: "Invalid user ID"
+    end
     @events = @user.events
   end
 end
