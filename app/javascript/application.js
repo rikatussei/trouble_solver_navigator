cebf9aaf8ba8d411d5a8c89c0c8a1da9f94e689e
@rikatussei <yohei.arisu@Gmail.com>
2024-07-18 13:50:42 +0900

    修正中

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,3 @@
-// app/javascript/application.js
 import Rails from "@rails/ujs";
 import { Application } from "@hotwired/stimulus";
 import { definitionsFromContext } from "stimulus/webpack-helpers";
@@ -7,9 +6,15 @@ import "controllers";
 import "./controllers/preview";
 import InfiniteScrollController from "./controllers/infinite_scroll_controller";
 
+// スティミュラスのロード
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
+
 Rails.start();  // Rails UJSの開始
 
 const application = Application.start();
 const context = require.context("controllers", true, /\.js$/);
 application.load(definitionsFromContext(context));
 application.register("infinite-scroll", InfiniteScrollController);
+
+// コントローラのイージーロード
+eagerLoadControllersFrom("controllers", application);
