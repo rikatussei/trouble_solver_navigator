a38c76cafeb5a6ed57fb5b445891d729a34f08f9
@rikatussei <yohei.arisu@Gmail.com>
2024-07-18 13:52:24 +0900

    もう一度修正

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,8 +1,13 @@
-// app/javascript/controllers/index.js
 import { application } from "controllers/application";
 import { Application } from "@hotwired/stimulus";
 import { definitionsFromContext } from "stimulus/webpack-helpers";
 
+// スティミュラスのロード
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
+
 const application = Application.start();
 const context = require.context(".", true, /\.js$/);
 application.load(definitionsFromContext(context));
+
+// コントローラのイージーロード
+eagerLoadControllersFrom(context, application);
