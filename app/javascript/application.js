bc3663ca1bf498ae0935315fbd978f026d6c4178
@rikatussei <yohei.arisu@Gmail.com>
2024-07-28 23:51:34 +0900

    記述を戻す

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,20 +1,21 @@
-import Rails from "@rails/ujs";
-Rails.start(); // Rails UJSの開始
-
+import Rails from "@rails/ujs";  // これが問題の行です
 import { Application } from "@hotwired/stimulus";
 import { definitionsFromContext } from "stimulus/webpack-helpers";
 import "@hotwired/turbo-rails";
+import "controllers";
+import "preview";
 import "./controllers/preview";
 import InfiniteScrollController from "./controllers/infinite_scroll_controller";
 
 // スティミュラスのロード
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
 
+Rails.start();  // Rails UJSの開始
+
 const application = Application.start();
 const context = require.context("controllers", true, /\.js$/);
+application.load(definitionsFromContext(context));
+application.register("infinite-scroll", InfiniteScrollController);
 
 // コントローラのイージーロード
 eagerLoadControllersFrom("controllers", application);
-
-application.load(definitionsFromContext(context));
-application.register("infinite-scroll", InfiniteScrollController);
